c98e2e40aad620ce86582b10298b677f38f33758
DawidMazur <dawid.mazur@icloud.com>
2021-10-23 00:12:32 +0200

    foundations of the getdata function, getting data from api

--- getdata.py
+++ getdata.py
@@ -4,3 +4,29 @@ import time
 from datetime import datetime
 
 
+def getdata(duration):
+
+    longitude_data = []
+    latitude_data = []
+    i = 0
+
+    while i < int(duration):
+        time.sleep(1)
+        url = 'http://api.open-notify.org/iss-now.json'
+        answer = requests.get(url)
+
+        if answer.status_code == 200:
+            answer = json.loads(answer.text)
+            position = answer['iss_position']
+            longitude = position['longitude']
+            latitude = position['latitude']
+            latitude_data.append(latitude)
+            longitude_data.append(longitude)
+            #print(latitude, longitude)
+
+        else:
+            print('Problem with status code')
+
+        i = i + 1
+
+    return longitude_data, latitude_data
