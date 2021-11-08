8f5de0bc1b848816ebf36b9e19f2fe92db13a652
DawidMazur <dawid.mazur@icloud.com>
2021-11-08 11:38:37 +0100

    Optimized and celaned code

--- analysis.py
+++ analysis.py
@@ -4,13 +4,12 @@ import getdata
 import numpy as np
 import pandas as pd
 import math
-import matplotlib as mpl
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
-from matplotlib.animation import FuncAnimation
 
 
 duration = int(input('Enter how many seconds the program should download data: '))
+print('\n')
 downloaded_data = getdata.getdata(duration)
 
 longitude_data = downloaded_data[0]
@@ -28,7 +27,7 @@ while i+1 < duration:
     distance_xi = float(latitude_data[i]) - float(latitude_data[i+1])
     if distance_xi < 0:
         distance_xi = distance_xi*(-1)
-    distance_x.append(round(distance_xi*1.852, 4))
+    distance_x.append(round(distance_xi*120.324, 3))
     i = i + 1
 
 distance_y = [NaN]
@@ -37,7 +36,7 @@ while i+1 < duration:
     distance_yi = float(longitude_data[i]) - float(longitude_data[i+1])
     if distance_yi < 0:
             distance_yi = distance_yi*(-1)
-    distance_y.append(round(distance_yi*1.852, 4))
+    distance_y.append(round(distance_yi*120.324, 3))
     i = i + 1
 
 distance_xy = [NaN]
@@ -47,37 +46,38 @@ while i < duration:
     distance_xy.append(distance_xyi)
     i = i + 1
 
-longitude_data_array = np.array(longitude_data)
-df1 = pd.DataFrame(longitude_data_array)
+df1 = pd.DataFrame(np.array(latitude_data))
 
-latitude_data_array = np.array(latitude_data)
-df2 = pd.DataFrame(latitude_data_array)
+df2 = pd.DataFrame(np.array(longitude_data))
     
-sample_time_array = np.array(sample_time_converted)
-df3 = pd.DataFrame(sample_time_array)
+df3 = pd.DataFrame(np.array(sample_time_converted))
 
-distance_x_array = np.array(distance_x)
-df4 = pd.DataFrame(distance_x_array)
+df4 = pd.DataFrame(np.array(distance_x))
 
-distance_y_array = np.array(distance_y)
-df5 = pd.DataFrame(distance_y_array)
+df5 = pd.DataFrame(np.array(distance_y))
 
 distance_xy_array = np.array(distance_xy)
-df6 = pd.DataFrame(distance_xy_array)
+df6 = pd.DataFrame(np.array(distance_xy))
 
 df = pd.concat([df1, df2, df3, df4, df5, df6], ignore_index=True, axis=1)
-df.columns = ['longitude', 'latitude', 'sample time', 'distance in axis x [km]', 'distance in axis y [km]', 'distance [km]']
-#print(df)
+df.columns = ['Latitude', 'Longitude', 'Sample time',
+ 'Distance in axis x [km]', 'Distance in axis y [km]', 'Distance [km]']
+print(df)
 
-x_values_d3 = [float(x) for x in longitude_data_array]
-y_values_d3 = [float(x) for x in latitude_data_array]
-z_values_d3 = np.array([i for i, _ in enumerate(latitude_data)])
+x_values_d3 = [float(x) for x in df['Latitude']]
+y_values_d3 = [float(y) for y in df['Longitude']]
+z_values_d3 = np.array([t for t, _ in enumerate(df['Sample time'])])
 
 fig = plt.figure()
 ax = fig.add_subplot(111, projection='3d')
-fig.set_size_inches(11, 11, forward=True)
-ax.plot3D(x_values_d3, y_values_d3, z_values_d3,edgecolor='lightgreen')
-ax.scatter(x_values_d3, y_values_d3, z_values_d3, '.r')
-plt.style.use('ggplot')
+fig.set_size_inches(12, 9, forward=True)
+ax.plot3D(x_values_d3, y_values_d3, z_values_d3,'g')
+ax.scatter(x_values_d3, y_values_d3, z_values_d3)
+plt.xlabel('Latitude')
+plt.ylabel('Longitude')
 plt.show()
 
+plt.plot(x_values_d3, y_values_d3)
+plt.xlabel('Latitude')
+plt.ylabel('Longitude')
+plt.show()
